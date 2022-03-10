a0ebd12ff991b375faf025c43f973ce04fedee06
Olga <55_olga_55@mail.ru>
2022-03-10 19:16:32 +0500

    Max Speed

---
+++ src/Statistics.java
@@ -0,0 +1,12 @@
+public class Statistics {
+    
+    public int max(int[] speeds) {
+        int max = speeds[0];
+        for (int speed : speeds) {
+            if (speed > max) {
+            }
+        }
+        return max;
+    }
+
+}
